7c812309b5724758a75973eeb1866fe573ddd2e4
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-09-11 10:40:58 +0900

    measure the performance of the case when a file is right under CWD

--- benchmark.js
+++ benchmark.js
@@ -9,7 +9,7 @@ const fsExtraOutputFile = require('fs-extra').outputFile;
 const outputFile = require('.');
 const rmfr = require('rmfr');
 
-const TIME = 5000;
+const TIME = 2000;
 
 async function measure(fn, title) {
 	const MAX_LENGTH = 39;
@@ -25,29 +25,39 @@ async function measure(fn, title) {
 	console.log(`${title}${((performance.now() - start) / (TIME / 1000)).toFixed(15).padStart(19)} ms/op avg.`);
 }
 
+const tmp = join(__dirname, 'tmp_benchmark');
+const contents = Buffer.from('Hi');
+
 (async () => {
-	const contents = Buffer.from('Hi');
-	const tmp = join(__dirname, 'tmp_benchmark');
 	let caseIndex = 0;
 
 	for (const [fnName, fn] of Object.entries({
 		'output-file (this project)': outputFile,
 		'fs-extra': fsExtraOutputFile
 	})) {
-		await mkdir(join(__dirname, 'tmp_benchmark'));
+		await mkdir(tmp);
 		console.log(`${fnName}:`);
 
 		await measure(
-			async index => fn(join(tmp, `out-${caseIndex}-${index}.txt`), contents),
+			async index => fn(join(tmp, `${caseIndex}-${index}.txt`), contents),
 			'Write a file to an existing directory'
 		);
+
+		process.chdir(tmp);
+
+		await measure(
+			async index => fn(join(tmp, `cwd-${caseIndex}-${index}.txt`), contents),
+			'Write a file to the current directory'
+		);
+
+		process.chdir(__dirname);
 
 		await measure(
 			async index => fn(join(tmp, `deep-${caseIndex}-${index}/0/1/2/3/4/5/6/7/8/9/out.txt`), contents),
 			'Create directories and write a file'
 		);
 
-		await rmfr(join(__dirname, 'tmp_benchmark'));
+		await rmfr(tmp);
 
 		caseIndex++;
 		console.log();
